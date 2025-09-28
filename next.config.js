7bc5320cd58ac72728c6eb51557e8f5af9e3a033
IzyGrow <izygroww@gmail.com>
2025-09-28 17:54:47 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
+    serverComponentsExternalPackages: ['pdf-parse'],
   },
 }
 
